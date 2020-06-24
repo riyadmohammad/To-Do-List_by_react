060e048a84831761454f807f44f77cd17ada0647
riyadmohammad <riyad.muhammad47@gmail.com>
2020-06-24 16:29:53 +0600

    react-and-bootstrap-first

---
+++ src/component/TodoList.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import Todoitems from './Todoitems';
+
+
+export default class TodoList extends Component {
+    render() {
+
+        const{items, clearList , handleDelete,handleEdit}=this.props;
+        return (
+
+           <ul className="list-group my-5">
+
+               <h3 className="text-capitalize text-center">todo list</h3>
+               
+               {
+
+                   items.map(item=>{
+
+                    return(
+                        <Todoitems 
+                         key={item.id}
+                         title={item.title} 
+                         handleDelete={()=> handleDelete(item.id)}
+                         handleEdit={()=> handleEdit(item.id)}
+                         
+                         />
+
+
+
+                    )
+
+
+                   })
+
+
+               }
+               
+              
+               <button type="button" className="btn btn-danger btn-block text-capitalize mt-5" onClick={clearList}>clear list</button>
+
+
+
+
+
+           </ul>
+            
+        )
+    }
+}
